9d1f11ee68843846027648127cd00207e677a6d7
Wojtek <wojjas@givingit.se>
2018-05-01 23:01:29 +0200

    Formatted document

--- Radhika/js/script.js
+++ Radhika/js/script.js
@@ -4,45 +4,45 @@ var rows = [{
     name: "Radhika Sonawane",
     education: "Web Developer CMS",
     city: "Malmö",
-    picture:"<img src='https://name.mynamepix.net/images/styles/itm_beautiful-redhead-doll_name_pix_2014-06-05_20-00-29_1.jpg' width='100px' height='100px'>"
+    picture: "<img src='https://name.mynamepix.net/images/styles/itm_beautiful-redhead-doll_name_pix_2014-06-05_20-00-29_1.jpg' width='100px' height='100px'>"
 
-},{
+}, {
     id: 02,
     name: "Sharvri Sonawane",
     education: "Computer science",
     city: "Lund",
-    picture:"<img src='http://1.bp.blogspot.com/-icACPnYx504/VEfuE0zEy_I/AAAAAAAABYw/dYziAFB8_mM/s1600/Pretty-Cute-Doll-240x320-Wallpaper.jpg' width='100px' height='100px'>"
-},{
+    picture: "<img src='http://1.bp.blogspot.com/-icACPnYx504/VEfuE0zEy_I/AAAAAAAABYw/dYziAFB8_mM/s1600/Pretty-Cute-Doll-240x320-Wallpaper.jpg' width='100px' height='100px'>"
+}, {
     id: 03,
     name: "Sara Nilson",
     education: "Bcs",
     city: "Oslo",
-    picture:"<img src='http://images6.fanpop.com/image/photos/35000000/cute-dolls-stya-E2-99-AC-35067989-176-220.jpg' width='100px' height='100px'>"
+    picture: "<img src='http://images6.fanpop.com/image/photos/35000000/cute-dolls-stya-E2-99-AC-35067989-176-220.jpg' width='100px' height='100px'>"
 }];
 
 var html = "<table border='1|1'>";
+
+html += "<tr>";
+html += "<th>id</th>";
+html += "<th>name</th>";
+html += "<th>education</th>";
+html += "<th>city</th>";
+html += "<th>picture</th>";
+
+html += "</tr>";
+
 for (var i = 0; i < rows.length; i++) {
-    
-    
-    html+="<tr>";
-    html+="<th>id</th>";
-    html+="<th>name</th>";
-    html+="<th>education</th>";
-    html+="<th>city</th>";
-    html+="<th>picture</th>";
 
-    html+="</tr>";
-    
-    html+="<tr>";
-    html+="<td>"+rows[i].id+"</td>";
-    html+="<td>"+rows[i].name+"</td>";
-    html+="<td>"+rows[i].education+"</td>";
-    html+="<td>"+rows[i].city+"</td>";
-    html+="<td>"+rows[i].picture+"</td>";
+    html += "<tr>";
+    html += "<td>" + rows[i].id + "</td>";
+    html += "<td>" + rows[i].name + "</td>";
+    html += "<td>" + rows[i].education + "</td>";
+    html += "<td>" + rows[i].city + "</td>";
+    html += "<td>" + rows[i].picture + "</td>";
 
 
-    html+="</tr>";
+    html += "</tr>";
 
 }
-html+="</table>";
+html += "</table>";
 document.getElementById("box").innerHTML = html;
--- Radhika/js/script1.js
+++ Radhika/js/script1.js
@@ -1,33 +1,33 @@
     function StudentTable() {
     //Build an array containing Student records.
-    var Students = new Array();
-    Students.push(["Student Id", "Name", "Education", "City", "Picture"]);
-    Students.push([1, "Radhika Sonawane", "BSC", "Malmo",  "<img src='https://name.mynamepix.net/images/styles/itm_beautiful-redhead-doll_name_pix_2014-06-05_20-00-29_1.jpg' width='100px' height='100px'>"]);
-    Students.push([2, "Kevin Johanson", "Bca", "Lund", "<img src='https://i.pinimg.com/originals/9d/3a/c5/9d3ac5ed43fddefe4000e4664be16744.jpg' width='100px' height='100px'>" ]);
-    Students.push([3, "Sara Jonson", "BE", "Oslo", "<img src='http://1.bp.blogspot.com/-icACPnYx504/VEfuE0zEy_I/AAAAAAAABYw/dYziAFB8_mM/s1600/Pretty-Cute-Doll-240x320-Wallpaper.jpg' width='100px' height='100px'>" ]);
-    Students.push([4, "Sharvri", "Bcs", "Lund", "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp60OEQKUA9VOAkUaN7nxYA_DMfumdTBdCTqfGX9TmdZo8P0A-' width='100px' height='100px'>"]);
+    var students = new Array();
+    students.push(["Student Id", "Name", "Education", "City", "Picture"]);
+    students.push([1, "Radhika Sonawane", "BSC", "Malmo",  "<img src='https://name.mynamepix.net/images/styles/itm_beautiful-redhead-doll_name_pix_2014-06-05_20-00-29_1.jpg' width='100px' height='100px'>"]);
+    students.push([2, "Kevin Johanson", "Bca", "Lund", "<img src='https://i.pinimg.com/originals/9d/3a/c5/9d3ac5ed43fddefe4000e4664be16744.jpg' width='100px' height='100px'>" ]);
+    students.push([3, "Sara Jonson", "BE", "Oslo", "<img src='http://1.bp.blogspot.com/-icACPnYx504/VEfuE0zEy_I/AAAAAAAABYw/dYziAFB8_mM/s1600/Pretty-Cute-Doll-240x320-Wallpaper.jpg' width='100px' height='100px'>" ]);
+    students.push([4, "Sharvri", "Bcs", "Lund", "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp60OEQKUA9VOAkUaN7nxYA_DMfumdTBdCTqfGX9TmdZo8P0A-' width='100px' height='100px'>"]);
  
     //Create a HTML Table element.
     var table = document.createElement("TABLE");
     table.border = "1";
  
     //Get the count of columns.
-    var columnCount = Students[0].length;
+    var columnCount = students[0].length;
  
     //Add the header row.
     var row = table.insertRow(-1);
     for (var i = 0; i < columnCount; i++) {
         var headerCell = document.createElement("TH");
-        headerCell.innerHTML = Students[0][i];
+        headerCell.innerHTML = students[0][i];
         row.appendChild(headerCell);
     }
  
     //Add the data rows.
-    for (var i = 1; i < Students.length; i++) {
+    for (var i = 1; i < students.length; i++) {
         row = table.insertRow(-1);
         for (var j = 0; j < columnCount; j++) {
             var cell = row.insertCell(-1);
-            cell.innerHTML = Students[i][j];
+            cell.innerHTML = students[i][j];
         }
     }
  
