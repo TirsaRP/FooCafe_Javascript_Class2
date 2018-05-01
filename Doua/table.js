3090e4dc338ca9bc805bae18d656dcf4127632d4
Wojtek <wojjas@givingit.se>
2018-05-01 23:04:44 +0200

    Formatted document

--- Doua/table.js
+++ Doua/table.js
@@ -1,85 +1,84 @@
-var students =[
-{
-    name: "Brad Pitt",
-    education:"General Medicine",
-    city:"Lund",
-    picture:  "https://drive.google.com/uc?id=11wyYBJMlCpcZ8UbrYcMnzcqz5Ei_5hzp" 
+var students = [
+    {
+        name: "Brad Pitt",
+        education: "General Medicine",
+        city: "Lund",
+        picture: "https://drive.google.com/uc?id=11wyYBJMlCpcZ8UbrYcMnzcqz5Ei_5hzp"
 
-},
-{
-    name: "George Clooney",
-    education:"Engineering",
-    city:"Malmö",
-    picture:  "https://drive.google.com/uc?id=1lqK0MS4X8pOgrVRhGwBJcMi3n7xiBl4j" 
+    },
+    {
+        name: "George Clooney",
+        education: "Engineering",
+        city: "Malmö",
+        picture: "https://drive.google.com/uc?id=1lqK0MS4X8pOgrVRhGwBJcMi3n7xiBl4j"
 
-},
-{
-    name: "Patrick Dempsey",
-    education:"Economy",
-    city:"Gutenberg",
-    picture:  "https://drive.google.com/uc?id=1PKZG8Vgma-B4WWKFfXjRyeAtcxpgyBZu" 
+    },
+    {
+        name: "Patrick Dempsey",
+        education: "Economy",
+        city: "Gutenberg",
+        picture: "https://drive.google.com/uc?id=1PKZG8Vgma-B4WWKFfXjRyeAtcxpgyBZu"
 
-}
+    }
 ]
 
 
-function getStudents (student){
+function getStudents(student) {
 
-var div = document.createElement("div")
-div.setAttribute("id","container")
-document.body.appendChild(div);
+    var div = document.createElement("div")
+    div.setAttribute("id", "container")
+    document.body.appendChild(div);
 
-var table = document.createElement('table');
-table.setAttribute("class","table");
-div.appendChild(table);
-
-
-var th1 = document.createElement("th");
-var th2 = document.createElement("th");
-var th3 = document.createElement("th");
-var th4 = document.createElement("th");
-
-th1.innerHTML = "Name";
-th2.innerHTML = "Education";
-th3.innerHTML = "City";
-th4.innerHTML = "Picture";
-
-table.appendChild(th1);
-table.appendChild(th2);
-table.appendChild(th3);
-table.appendChild(th4);
+    var table = document.createElement('table');
+    table.setAttribute("class", "table");
+    div.appendChild(table);
 
 
+    var th1 = document.createElement("th");
+    var th2 = document.createElement("th");
+    var th3 = document.createElement("th");
+    var th4 = document.createElement("th");
 
-for (var i = 0; i < student.length; i++){
+    th1.innerHTML = "Name";
+    th2.innerHTML = "Education";
+    th3.innerHTML = "City";
+    th4.innerHTML = "Picture";
 
-    var tr = document.createElement('tr'); 
-    var td1 = document.createElement('td');
-    var td2 = document.createElement('td');
-    var td3 = document.createElement('td');
-    var div1 = document.createElement("div")
-div.setAttribute("id","imgStyle")
-document.body.appendChild(div);
-    var image = document.createElement("img");
-    image.setAttribute("src", student[i].picture);
-    image.setAttribute("width", "150");
-    image.setAttribute("height", "150");
-    image.setAttribute("alt", student[i].name);
-    div1.appendChild(image);
+    table.appendChild(th1);
+    table.appendChild(th2);
+    table.appendChild(th3);
+    table.appendChild(th4);
 
-    td1.innerHTML = student[i].name;
-    td2.innerHTML = student[i].education;
-    td3.innerHTML = student[i].city;
-    
 
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-    
-    tr.appendChild(image);
+    for (var i = 0; i < student.length; i++) {
 
-    table.appendChild(tr);
-}
+        var tr = document.createElement('tr');
+        var td1 = document.createElement('td');
+        var td2 = document.createElement('td');
+        var td3 = document.createElement('td');
+        var div1 = document.createElement("div")
+        div.setAttribute("id", "imgStyle")
+        document.body.appendChild(div);
+        var image = document.createElement("img");
+        image.setAttribute("src", student[i].picture);
+        image.setAttribute("width", "150");
+        image.setAttribute("height", "150");
+        image.setAttribute("alt", student[i].name);
+        div1.appendChild(image);
+
+        td1.innerHTML = student[i].name;
+        td2.innerHTML = student[i].education;
+        td3.innerHTML = student[i].city;
+
+
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+
+        tr.appendChild(image);
+
+        table.appendChild(tr);
+    }
 }
 
 
